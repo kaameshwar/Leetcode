59f7421fbc7120348bc415dbbaf79952dc4045b6
kaameshwar <kaameshwar.s.2025.it@rajalakshmi.edu.in>
2026-07-08 02:27:39 +0530

    Time: 2 ms (42.07%), Space: 94.6 MB (15.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,6 +2,7 @@ class Solution {
     public int maxProfit(int[] prices) {
         int result = 0;
         int left=0, right=1;
+        
 
         while(right<prices.length){
             if(prices[right]<prices[left]){
