4111f30788737e3130ff53181fad69e1375ce08f
kaameshwar <kaameshwar.s.2025.it@rajalakshmi.edu.in>
2026-07-11 09:48:33 +0530

    Time: 0 ms (100%), Space: 43.2 MB (42.96%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -1,19 +1,22 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         int count=0;
-        for (int i =0; i<nums.length-2;i++){
-            int diff = nums[i+1]-nums[i];
-            for (int j =i+2; j<nums.length;j++){
-                if(nums[j]-nums[j-1]==diff && (j-i)+1 >=3){
-                    count++;
-                }
-                else{
-                    break;
-                }
-
-            }
+        if(nums.length<3){
+            return 0;
         }
-        return count++;
-        
+        int diff = nums[1]-nums[0];
+        int left =0;
+        for (int right =2; right<nums.length;right++){
+            if(nums[right]-nums[right-1]== diff){
+                count+= right-left-1;
+            }
+            else{
+                diff = nums[right]-nums[right-1];
+                left = right-1;
+            }
+            
+            
+        }
+        return count;
     }
 }
