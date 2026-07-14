cd9d187c71f3064ca65e0b5f0677373c27e470ba
kaameshwar <kaameshwar.s.2025.it@rajalakshmi.edu.in>
2026-07-14 14:38:06 +0530

    Time: 110 ms (25.07%), Space: 71.7 MB (7.68%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        int [] array= new int[n+1];
+        int count =0;
+
+        for (int i =2;i<n;i++){
+            array[i]=1;
+        }
+        for(int j =2;j*j<=n;j++){
+            if(array[j]==1){
+                for(int k =2*j;k<=n;k+=j){
+                    array[k]=0;
+                }
+            }
+        }
+        for(int iterator =2;iterator<=n;iterator++){
+            if(array[iterator]==1){
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
