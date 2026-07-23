dc688f2a6004bfb0303c08238744faf264efaafa
kaameshwar <kaameshwar.s.2025.it@rajalakshmi.edu.in>
2026-07-23 20:06:33 +0530

    Time: 15 ms (10.41%) | Memory: 47.4 MB (5.07%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLongestSubstring(String str) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        int left =0;
+        int answer = 0;
+        for (int right =0; right<str.length();right++){
+            if(map.containsKey(str.charAt(right))){
+                while(map.containsKey(str.charAt(right))){
+                    map.put(str.charAt(left),map.get(str.charAt(left))-1);
+
+                    if(map.get(str.charAt(left))==0){
+                        map.remove(str.charAt(left));
+                    }
+                    left++;
+                }
+            }
+            map.put(str.charAt(right), map.getOrDefault(str.charAt(right), 0)+1);
+
+            answer = Math.max(answer,(right-left)+1);
+        }
+        return answer;
+    }
+}
