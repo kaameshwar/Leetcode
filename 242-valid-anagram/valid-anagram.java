02c59165894fba20446693deaf0a2a86d1ac1280
kaameshwar <kaameshwar.s.2025.it@rajalakshmi.edu.in>
2026-07-27 00:25:05 +0530

    Time: 8 ms (30.23%) | Memory: 44.1 MB (90.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,28 +1,16 @@
 class Solution {
-    public boolean isAnagram(String string1, String string2) {
-        int count =0;
-        if(string1.length()!= string2.length()){
-            return false;
-        }
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+    public boolean isAnagram(String str1, String str2) {
+        int []frequency = new int [26];
+        if (str1.length()!= str2.length()) return false;
 
-        for (int i =0; i<string1.length(); i++){
-            char c1 = string1.charAt(i);
-            char c2 = string2.charAt(i);
-            map1.put(c1, map1.getOrDefault(c1, 0) + 1);
-            map2.put(c2, map2.getOrDefault(c2, 0) + 1);
-            
-        }
-        //for (int i =0; i<string1.length();i++){
-            //char c1 = string1.charAt(i);
-            //if(map1.get(c1)==map2.get(c1)){
-                //count++;
-            //}
-        //}
-        //if(count == string1.length()) return true;
+        for (int pointer =0 ; pointer< str1.length(); pointer++){
+            frequency[str1.charAt(pointer)- 'a']++;
+            frequency[str2.charAt(pointer)- 'a']--;
 
-        //return false;
-        return map1.equals(map2);
+        }
+        for (int number : frequency){
+            if(number!=0) return false;
+        }
+        return true;
     }
 }
