087ff29751afbe9ff8a37894579b66b47308d6ad
kaameshwar <kaameshwar.s.2025.it@rajalakshmi.edu.in>
2026-07-27 13:05:24 +0530

    Time: 35 ms (44.10%) | Memory: 59.1 MB (60.07%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -5,6 +5,7 @@ class Solution {
         int sum =0;
         int n = nums.length;
         for (int i =0; i<n-2;i++){
+            if(i>0 && nums[i]== nums[i-1]) continue;
             int left = i+1, right = n-1;
             while(left<right){
                 sum = nums[i]+nums[left]+nums[right];
@@ -12,6 +13,9 @@ class Solution {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
+                
+                while(left<right && nums[right]== nums[right+1]) {right--;}
+                while(left<right && nums[left]== nums[left-1]) {left++;}
                 }
                 else if(sum>0){
                     right--;
@@ -19,11 +23,10 @@ class Solution {
                 else{
                     left++;
                 }
+                
             }
         }
-        Set<List<Integer>> set = new LinkedHashSet<>(result);
-        result = new ArrayList<>(set);
-
+    
         return result;
     }
 }
