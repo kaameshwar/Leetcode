8f3800256a43ef69bcace4f094d0306c8c673930
kaameshwar <kaameshwar.s.2025.it@rajalakshmi.edu.in>
2026-08-09 14:57:21 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (70.17%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        
+        int left =0;
+        int right =n-1;
+        if(n==1){
+            if(nums[0]== target) return 0;
+            else return -1;
+
+        }
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]== target){
+                return mid;
+
+            }
+            else if (target>nums[mid]){
+                left= mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+}
