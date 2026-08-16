eea03b1303562114693f0dcc2628a74c61475cbf
kaameshwar <kaameshwar.s.2025.it@rajalakshmi.edu.in>
2026-08-16 16:50:51 +0530

    Time: 26 ms (14.41%) | Memory: 47.9 MB (59.59%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,28 +1,30 @@
 class Solution {
-    int total_hours(int[] piles, int hours){
+    int total_hours(int [] piles, int speed){
         int n = piles.length;
-        int total_hours=0;
-        for(int i =0; i<n;i++){
-            total_hours+=Math.ceil((double)piles[i]/(double)hours);
+        int total_hours =0;
+        for (int i =0; i<n;i++){
+            total_hours+=Math.ceil((double)piles[i]/ (double)speed);
         }
         return total_hours;
     }
     public int minEatingSpeed(int[] piles, int h) {
         int n = piles.length;
-        int left =1;
-        int max =0;
-        for (int i=0; i<n;i++){
-            max = Math.max(max, piles[i]);
+        int largest_number =0;
+        for (int i =0 ;i<n;i++){
+            largest_number = Math.max(largest_number, piles[i]);
         }
-        int right = max;
+        int left =1;
+        int right = largest_number;
+        int hours_taken=0;
         while(left<=right){
-            int mid= left+(right-left)/2;
-            int hours = total_hours(piles, mid);
-            if(hours<=h){
-                right= mid-1;
-            }
-            else{
+            int mid = left+(right-left)/2;
+            hours_taken = total_hours(piles, mid);
+
+            if(hours_taken > h){
                 left = mid+1;
+            }
+            else {
+                right = mid-1;
             }
         }
         return left;
